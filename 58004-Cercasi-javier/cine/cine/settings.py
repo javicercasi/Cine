c300aa0b83195e24ac7055922e45ee964e80bc07
Javi Cercasi <jacercasii@gmail.com>
2020-12-04 13:18:03 -0300

    local

--- 58004-Cercasi-javier/cine/cine/settings.py
+++ 58004-Cercasi-javier/cine/cine/settings.py
@@ -118,9 +118,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = False
--- 58004-Cercasi-javier/cine/proyecto/admin.py
+++ 58004-Cercasi-javier/cine/proyecto/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Pelicula, Sala, Proyeccion, Reserva
+
 
 # Register your models here.
+admin.site.register(Pelicula)
+admin.site.register(Sala)
+admin.site.register(Proyeccion)
+admin.site.register(Reserva)
