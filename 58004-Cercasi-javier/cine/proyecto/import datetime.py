474a803dd51fcf1014a7e68a11b0e51ae9d2e798
Javi Cercasi <jacercasii@gmail.com>
2020-12-04 21:10:59 -0300

    local

---
+++ 58004-Cercasi-javier/cine/proyecto/import datetime.py
@@ -0,0 +1,6 @@
+from datetime import datetime, timedelta, date
+import datetime as dt
+a = dt.timedelta(days=10)
+b = dt.datetime.now()
+fecha = date.today()
+print(fecha)
--- 58004-Cercasi-javier/cine/proyecto/urls.py
+++ 58004-Cercasi-javier/cine/proyecto/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 from proyecto import views
 
 urlpatterns = [
-    url(r'^api/proyecto$', views.pelicula_list),
-    url(r'^api/proyecto/(?P<pk>[0-9]+)$', views.pelicula_detail), #pk=como un primary key, que lo tome literal, y llame a la funcion view.pelicula
-    url(r'^api/proyecto/published$', views.pelicula_list_published)
+
+    url(r'^api/peliculas$', views.peliculas_list),
+    url(r'^api/peliculas/(?P<pk>[0-9]+)$', views.pelicula_detail),
+    url(r'^api/salas$', views.salas_list),
+    url(r'^api/salas/(?P<pk>[0-9]+)$', views.salas_detail),
+    url(r'^api/proyecciones$', views.proyecciones_list)
+
 ]
