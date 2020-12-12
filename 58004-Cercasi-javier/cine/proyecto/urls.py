1aa33b905415881e5aade74c169e7b017a1ee27e
Javi Cercasi <jacercasii@gmail.com>
2020-12-12 14:09:29 -0300

    local

--- 58004-Cercasi-javier/cine/proyecto/urls.py
+++ 58004-Cercasi-javier/cine/proyecto/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
     url(r'^api/proyecciones$', views.proyecciones_list),
     url(r'^api/proyecciones/(?P<pk>[0-9]+)$', views.proyecciones_detail),
     url(r'^api/butacas$', views.butacas_list),
-    url(r'^api/butacas/(?P<pk>[0-9]+)$', views.butacas_detail)
+    url(r'^api/butacas/(?P<pk>[0-9]+)$', views.butacas_detail),
+    url(r'^api/reportes$', views.reportes_list),
+    url(r'^api/reportes/(?P<pk>[0-9]+)$', views.reportes_list),
+    url(r'^api/reportes/ranking$', views.reportes_ranking),
+    url(r'^api/reportes/peliculas$', views.reporte_peliculas)
 ]
